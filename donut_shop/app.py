0ca29f3acccb8aeacda8cd3084bb8eebf61ad7a3
seanthony <sean@basecampcodingacademy.org>
2019-08-13 13:50:36 -0500

    user selects topping in donut app

--- donut_shop/app.py
+++ donut_shop/app.py
@@ -12,6 +12,14 @@ def open_donut_file(filename):
     return toppings, flavors
 
 
+def get_topping(toppings):
+    while True:
+        topping = input(' ? >>> ').strip().lower()
+        if topping in toppings:
+            return topping
+        print('Sorry!', topping, 'is not an option')
+
+
 def donut_shop():
     toppings, flavors = open_donut_file(DONUT_FILENAME)
     print('''Hello! Welcome to the Donut Shop!
@@ -20,6 +28,10 @@ def donut_shop():
     
     Choose your topping and flavor! Or get a random flavor!
     ''')
+
+    print('What topping would you like?')
+    print(' | '.join(toppings))
+    topping = get_topping(toppings)
 
 
 if __name__ == '__main__':
