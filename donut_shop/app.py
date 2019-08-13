a53b4a33d741fe9040a993b83a1576a2fd784f9b
seanthony <sean@basecampcodingacademy.org>
2019-08-13 13:47:31 -0500

    open and load json in file. changed one of the keys in the JSON file

--- donut_shop/app.py
+++ donut_shop/app.py
@@ -1,5 +1,25 @@
+import json
+
+DONUT_FILENAME = './donut_shop/donuts.json'
+
+
+def open_donut_file(filename):
+    with open(filename, 'r') as file:
+        donut_dictionary = json.load(file)
+
+    toppings = donut_dictionary.get('toppings')
+    flavors = donut_dictionary.get('flavors')
+    return toppings, flavors
+
+
 def donut_shop():
-    print('hello world')
+    toppings, flavors = open_donut_file(DONUT_FILENAME)
+    print('''Hello! Welcome to the Donut Shop!
+    
+    All donuts are $1, cash only.
+    
+    Choose your topping and flavor! Or get a random flavor!
+    ''')
 
 
 if __name__ == '__main__':
